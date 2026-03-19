7c099a0dda68b06f550a087b833503a06546da5f
ShizuK8 <jurgen20112024@gmail.com>
2026-03-19 14:43:20 +0100

    script change

--- 2D platform Game/Assets/Script/Obstacle.cs
+++ 2D platform Game/Assets/Script/Obstacle.cs
@@ -3,9 +3,12 @@ using UnityEngine;
 public class Obstacle : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
+    float minSize = 0.75f;
+    float maxSize = 3f;
     void Start()
     {
-        transform.localScale = new Vector3(2, 2, 1);
+        float randomSize = Random.Range(minSize, maxSize);
+        transform.localScale = new Vector3(randomSize, randomSize, 1);
     }
 
     // Update is called once per frame
