7d08217a818386e2dbd1589bf1f8bcf9c988c342
ShizuK8 <jurgen20112024@gmail.com>
2026-03-20 09:11:52 +0100

    random speed script

--- 2D platform Game/Assets/Script/Obstacle.cs
+++ 2D platform Game/Assets/Script/Obstacle.cs
@@ -5,10 +5,17 @@ public class Obstacle : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     float minSize = 0.75f;
     float maxSize = 3f;
+    Rigidbody2D rb;
+    float minSpeed = 50f;
+    float maxSpeed = 150f;
+
     void Start()
     {
         float randomSize = Random.Range(minSize, maxSize);
         transform.localScale = new Vector3(randomSize, randomSize, 1);
+        rb = GetComponent<Rigidbody2D>();
+        rb.AddForce(Vector2.right * randomSpeed);
+        float randomSpeed = Random.Range(minSpeed, maxSpeed);
     }
 
     // Update is called once per frame
